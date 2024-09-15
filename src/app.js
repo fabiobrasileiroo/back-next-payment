32c7f2b42b887fe18e390970ee17cd8238e1bb49
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-09-15 10:38:12 -0400

    remove

--- src/app.js
+++ src/app.js
@@ -12,25 +12,3 @@ app.use('/api/payments', paymentRoutes)
 
 export default app
 
-// import 'dotenv/config'
-// import { MercadoPagoConfig, Payment } from 'mercadopago'
-
-// const client = new MercadoPagoConfig({
-//   accessToken: process.env.accessToken,
-//   options: {
-//     timeout: 5000,
-//   },
-// })
-
-// const payment = new Payment(client)
-
-// const body = {
-//   transaction_amount: 30.5,
-//   description: 'Teste api pix V2',
-//   payment_method_id: 'pix',
-//   payer: {
-//     email: 'fabio.h591@gmail.com',
-//   },
-// }
-
-// payment.create({ body }).then(console.log).catch(console.log)
--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const PORT = process.env.PORT || 3000
 
 const startServer = async () => {
   try {
-    // Conectar ao banco de dados PostgreSQL (se for necessário conectar a um banco local para testes)
-    // await mongoose.connect(process.env.MONGO_URI);
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
